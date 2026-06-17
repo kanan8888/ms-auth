4672d8842bb500a546a5b25b11d8376868c9e456
Kanan_Eyvazli <kanan.eyvazli.11@gmail.com>
2026-06-17 20:07:48 +0400

    Application Setup

--- src/main/java/az/authservice/Application.java
+++ src/main/java/az/authservice/Application.java
@@ -1,13 +1,17 @@
 package az.authservice;
 
+import az.authservice.configuration.properties.JwtProperties;
+import az.authservice.configuration.properties.RabbitMQProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableConfigurationProperties({JwtProperties.class, RabbitMQProperties.class})
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
