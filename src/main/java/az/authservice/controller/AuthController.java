f273fc00b32d510f98e692966308c93e90857aa8
Kanan_Eyvazli <kanan.eyvazli.11@gmail.com>
2026-06-19 12:30:46 +0400

    Authentication API & Controller

--- src/main/java/az/authservice/controller/AuthController.java
+++ src/main/java/az/authservice/controller/AuthController.java
@@ -6,7 +6,6 @@ import az.authservice.dto.response.ValidateResponse;
 import az.authservice.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
